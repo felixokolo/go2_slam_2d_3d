b37f3f9e67416059f5638fc109cc88e390cee8ec
felixokolo <pheelix.info@gmail.com>
2025-02-25 21:19:45 +0100

    Included missing build command for hesai

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ colcon build --packages-select go2_joints_state_publisher --cmake-args -DCMAKE_B
 colcon build --packages-select go2_rviz_display
 colcon build --packages-select pointcloud_to_laserscan --cmake-args -DCMAKE_BUILD_TYPE=Release
 colcon build --packages-select odom_to_tf_ros2 --cmake-args -DCMAKE_BUILD_TYPE=Release
-colcon build --packages-select lio_sam
+colcon build --packages-select lio_sam --cmake-args -DCMAKE_BUILD_TYPE=Release
+colcon build --packages-select hesai_lidar --cmake-args -DCMAKE_BUILD_TYPE=Release
